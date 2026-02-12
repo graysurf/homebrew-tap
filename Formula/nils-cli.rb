b895dce72dd60e596c644dbcfa27de7ea03c62d5
graysurf <10785178+graysurf@users.noreply.github.com>
2026-02-13 04:53:21 +0800

    chore(formula): bump nils-cli to v0.3.5
    - Update Release asset URLs for macOS and Linux targets
    - Refresh SHA256 checksums from graysurf/nils-cli v0.3.5 artifacts

--- Formula/nils-cli.rb
+++ Formula/nils-cli.rb
@@ -5,21 +5,21 @@ class NilsCli < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/graysurf/nils-cli/releases/download/v0.3.3/nils-cli-v0.3.3-aarch64-apple-darwin.tar.gz"
-      sha256 "0b8584e464ebe3c2b269f2a34021538c6c362290fe5df2bc7d4f641169459980"
+      url "https://github.com/graysurf/nils-cli/releases/download/v0.3.5/nils-cli-v0.3.5-aarch64-apple-darwin.tar.gz"
+      sha256 "e9675a75b52c6ce3c2aec4d3959577d40c81fa9902869693b3dd101341c32ab7"
     else
-      url "https://github.com/graysurf/nils-cli/releases/download/v0.3.3/nils-cli-v0.3.3-x86_64-apple-darwin.tar.gz"
-      sha256 "f5f3638171d72076c628ffb713cf475f4349b3ac6920e15d5a83d2815cbaa6d5"
+      url "https://github.com/graysurf/nils-cli/releases/download/v0.3.5/nils-cli-v0.3.5-x86_64-apple-darwin.tar.gz"
+      sha256 "d2f95f5a4a59940035273039654ae0ddea11cabc6afd70e6097df459b76ea128"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/graysurf/nils-cli/releases/download/v0.3.3/nils-cli-v0.3.3-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "dc48adf815fa1afc4123c4bcb1b47c67aff6b28d0699c5eeaa6666fdf1eca76f"
+      url "https://github.com/graysurf/nils-cli/releases/download/v0.3.5/nils-cli-v0.3.5-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "97b9449db608de810e191b29b30bc0b15d28df5d6e25c9c964ca58e99a824a0b"
     else
-      url "https://github.com/graysurf/nils-cli/releases/download/v0.3.3/nils-cli-v0.3.3-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "2377ae127f8e36fe0ba3f2ac1465b14604d3c3182de94d4a0255f9885eedcc10"
+      url "https://github.com/graysurf/nils-cli/releases/download/v0.3.5/nils-cli-v0.3.5-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "143cb51b64a1f820890bed579633101f7b922ad2f19816fc67293828db125677"
     end
   end
 

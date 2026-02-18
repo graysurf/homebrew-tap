2b05b0883b21f25823cca8f22cb3f33920b867eb
graysurf <10785178+graysurf@users.noreply.github.com>
2026-02-18 23:14:37 +0800

    chore(formula): bump agent-workspace-launcher to v1.1.5

--- Formula/agent-workspace-launcher.rb
+++ Formula/agent-workspace-launcher.rb
@@ -1,26 +1,26 @@
 class AgentWorkspaceLauncher < Formula
   desc "Host-native workspace lifecycle CLI"
   homepage "https://github.com/graysurf/agent-workspace-launcher"
-  version "1.1.4"
+  version "1.1.5"
   license "MIT"
   on_macos do
     on_arm do
-      url "https://github.com/graysurf/agent-workspace-launcher/releases/download/v1.1.4/agent-workspace-launcher-v1.1.4-aarch64-apple-darwin.tar.gz"
-      sha256 "6bf446fb756c8c4c6f6fe19c0d91121421c3a42fad7270cb533b53e363ddb575"
+      url "https://github.com/graysurf/agent-workspace-launcher/releases/download/v1.1.5/agent-workspace-launcher-v1.1.5-aarch64-apple-darwin.tar.gz"
+      sha256 "b86088a45ff2abbebb4ad56fa9449ae83bddeb47fd2f3e3217286030b25f7166"
     end
     on_intel do
-      url "https://github.com/graysurf/agent-workspace-launcher/releases/download/v1.1.4/agent-workspace-launcher-v1.1.4-x86_64-apple-darwin.tar.gz"
-      sha256 "b994c291bf7e072d5c8881147f1be7682bd7f2f50dffaa0cbc98c21c076e6a87"
+      url "https://github.com/graysurf/agent-workspace-launcher/releases/download/v1.1.5/agent-workspace-launcher-v1.1.5-x86_64-apple-darwin.tar.gz"
+      sha256 "d5d2489aea01dcd403c3bb3fa7339729fa0ed6b58eab8be4fe87542cbe4c6d0b"
     end
   end
   on_linux do
     on_arm do
-      url "https://github.com/graysurf/agent-workspace-launcher/releases/download/v1.1.4/agent-workspace-launcher-v1.1.4-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "562632dc814b679e03847c1e32c67683d76fc0dd74095d83fbad3cffd07455b9"
+      url "https://github.com/graysurf/agent-workspace-launcher/releases/download/v1.1.5/agent-workspace-launcher-v1.1.5-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "cedbf17f77eced36aabe2789bf47476020bb6adbe3e6183b70b5e240afa9d785"
     end
     on_intel do
-      url "https://github.com/graysurf/agent-workspace-launcher/releases/download/v1.1.4/agent-workspace-launcher-v1.1.4-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "fa5b38a5643b5e92af86c93f1981436afa21d20580585cc1dfa8bcb76a979dbf"
+      url "https://github.com/graysurf/agent-workspace-launcher/releases/download/v1.1.5/agent-workspace-launcher-v1.1.5-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "82e953bfa09b4fb33eecb409d4a1d03d2a93829a9c097a17f4e907264e3b49a5"
     end
   end
   def install
